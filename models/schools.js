d6f8cb178d99940748ab614eb7d943baad1d2f76
Ishan Varshney <ishanvarshney99@gmail.com>
2020-02-03 23:41:10 +0530

    bugs fixed

--- models/schools.js
+++ models/schools.js
@@ -15,28 +15,36 @@ var schoolSchema=new mongoose.Schema({
 	}]
 })
 var School=mongoose.model('School',schoolSchema);
-School.create({
-	abbr:"soict"
-})
-School.create({
-	abbr:"sob"
-})
-School.create({
-	abbr:"sobs"
-})
-School.create({
-	abbr:"sovs"
-})
-School.create({
-	abbr:"soh"
-})
-School.create({
-	abbr:"soe"
-})
-School.create({
-	abbr:"som"
-})
-School.create({
-	abbr:"soljg"
-})
+// School.create({
+// 	abbr:"soict",
+// 	name:"ICT"
+// })
+// School.create({
+// 	abbr:"sob",
+// 	name:"Biotech"
+// })
+// School.create({
+// 	abbr:"sobs",
+// 	name:"Buddhism"
+// })
+// School.create({
+// 	abbr:"sovs",
+// 	name:"Vocational"
+// })
+// School.create({
+// 	abbr:"soh",
+// 	name:"Humanities"
+// })
+// School.create({
+// 	abbr:"soe",
+// 	name:"Engineering"
+// })
+// School.create({
+// 	abbr:"som",
+// 	name:"Management"
+// })
+// School.create({
+// 	abbr:"soljg",
+// 	name:"Law"
+// })
 module.exports=School;
